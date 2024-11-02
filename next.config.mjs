7324061266c79d63f376d618fd675798d6f0185d
Petr Balat <petr.balat@gmail.com>
2024-11-02 21:37:00 +0100

    mdx - https://nextjs.org/docs/pages/building-your-application/configuring/mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+import createMDX from '@next/mdx'
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  /* config options here */
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
+export default withMDX(nextConfig)
