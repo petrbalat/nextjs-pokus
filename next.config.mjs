192d1b2dd527f864076c7c41776e4c65ada2ea1f
Petr Balat <petr.balat@gmail.com>
2025-02-02 09:04:25 +0100

    tailwind 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
-import createMDX from '@next/mdx'
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  /* config options here */
 };
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-})
-
-export default withMDX(nextConfig)
+export default nextConfig;
--- src/app/markdown/remote/page.tsx
+++
@@ -1,8 +0,0 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
-
-export default async function RemoteMdxPage() {
-  // MDX text - can be from a local file, database, CMS, fetch, anywhere...
-  const res = await fetch('https://raw.githubusercontent.com/vercel/next.js/refs/heads/canary/packages/next-mdx/readme.md')
-  const markdown = await res.text()
-  return <MDXRemote source={markdown} />
-}
